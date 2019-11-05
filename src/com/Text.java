f29b6348a1179d479b24df908e5850125b3ccca3
linyu001 <1923477173@qq.com>
2019-11-05 13:46:14 +0800

    测试

---
+++ src/com/Text.java
@@ -0,0 +1,13 @@
+package com;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Text {
+	public static void main(String[] args) {
+		SimpleDateFormat sip =new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String time =sip.format(new Date());
+	    System.out.println(time);
+	}
+
+}
